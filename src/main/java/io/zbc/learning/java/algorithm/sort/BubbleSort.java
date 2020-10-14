94217cefdccbe0a08715c8e599a6de251721f066
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-14 16:42:14 +0800

    Update BubbleSort.java

--- src/main/java/io/zbc/learning/java/algorithm/sort/BubbleSort.java
+++ src/main/java/io/zbc/learning/java/algorithm/sort/BubbleSort.java
@@ -12,13 +12,15 @@ public class BubbleSort implements Sort {
     public int[] sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
+            System.out.println("----------------第" + (i + 1) + "次排序----------------");
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
+                System.out.println("第" + (i + 1) + "次排序的第" + (j + 1) + "次比较：" + Arrays.toString(array));
             }
-            System.out.println(Arrays.toString(array));
+            System.out.println("第" + (i + 1) + "次排序结果：" + Arrays.toString(array));
         }
         return array;
     }
