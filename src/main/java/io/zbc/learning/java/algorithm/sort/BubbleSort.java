7620c7fcbfb3af7d430daf3d6aa41fe17b06f6d6
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-14 16:44:11 +0800

    Update BubbleSort.java

--- src/main/java/io/zbc/learning/java/algorithm/sort/BubbleSort.java
+++ src/main/java/io/zbc/learning/java/algorithm/sort/BubbleSort.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 public class BubbleSort implements Sort {
     public static void main(String[] args) {
         int[] array = {5, 3, 2, 6, 1, 4};
+//        int[] array = {1, 2, 3, 5, 4};
+//        int[] array = {2, 1, 3, 4, 5, 6, 7};
         new BubbleSort().sort(array);
     }
 
     @Override
     public int[] sort(int[] array) {
+        int temp = 0;
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
             System.out.println("----------------第" + (i + 1) + "次排序----------------");
                 if (array[j] > array[j + 1]) {
-                    int temp = array[j];
+                    temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
