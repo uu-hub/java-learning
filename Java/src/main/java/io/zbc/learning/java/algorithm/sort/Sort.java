e19dcb25b47c093b7faf5f681dd614941018bb88
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2021-03-31 11:11:27 +0800

    删除目录

--- Java/src/main/java/io/zbc/learning/java/algorithm/sort/Sort.java
+++
@@ -1,5 +0,0 @@
-package io.zbc.learning.java.algorithm.sort;
-
-public interface Sort {
-    public int[] sort(int[] array);
-}
--- Spring/spring-data/spring-data-redis/src/main/java/io/zbc/learning/spring/data/redis/service/IRedisService.java
+++
@@ -1,7 +0,0 @@
-package io.zbc.learning.spring.data.redis.service;
-
-public interface IRedisService {
-
-    public String setValue(String key) throws InterruptedException;
-    public String getValue(String key);
-}
