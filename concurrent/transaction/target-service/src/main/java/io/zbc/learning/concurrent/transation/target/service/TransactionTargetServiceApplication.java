09f41a6819d61e67e0030b138cb23ab09ed2808d
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2021-08-18 18:21:09 +0800

    事务学习项目创建

---
+++ concurrent/transaction/target-service/src/main/java/io/zbc/learning/concurrent/transation/target/service/TransactionTargetServiceApplication.java
@@ -0,0 +1,13 @@
+package io.zbc.learning.concurrent.transation.target.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTargetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionTargetServiceApplication.class, args);
+    }
+
+}
---
+++ concurrent/transaction/target-service/src/test/java/io/zbc/learning/concurrent/transation/target/service/TransactionTargetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zbc.learning.concurrent.transation.target.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionTargetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
